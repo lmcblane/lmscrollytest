68b648b043921aa6425d882e48c91d2837986d55
lmcblane <lewis.mcblane@hnmedia.co.uk>
2025-09-23 16:29:05 +0100

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ style.textContent = `
   * { margin: 0; padding: 0; box-sizing: border-box; }
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
-    background: #0a0a0a;
     overflow-x: hidden;
     /* remove forced color:white here */
   }
